5171e2f202cfbafc2ec0d61627b396e9ff56a147
EC2 Default User <ec2-user@ip-172-31-9-85.ap-northeast-1.compute.internal>
2023-06-08 15:02:01 +0000

    /spec/test_spec.rb　簡単なテストに変更

--- spec/test_spec.rb
+++ spec/test_spec.rb
@@ -1,20 +1,5 @@
 require 'spec_helper'
 
-describe stack('sample-stack') do
-  it { should exist }
-  it { should be_running }
-  its('stack_status') { should eq 'CREATE_COMPLETE' }
-  its('template_description') { should include 'AWS CloudFormation Sample Template' }
-  its('parameters') do
-    should include(
-      {
-        'ParameterKey' => 'InstanceType',
-        'ParameterValue' => 't2.micro'
-      },
-      {
-        'ParameterKey' => 'KeyName',
-        'ParameterValue' => 'my-key-pair'
-      }
-    )
-  end
+describe package('git') do
+  it { should be_installed }
 end
