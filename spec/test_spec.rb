01219a7a9255372499e423ff341ee945f818213a
EC2 Default User <ec2-user@ip-172-31-9-85.ap-northeast-1.compute.internal>
2023-06-08 14:59:02 +0000

    /spec/test_spec.rb　テストを変更

--- spec/test_spec.rb
+++ spec/test_spec.rb
@@ -1,29 +1,20 @@
 require 'spec_helper'
 
-describe 'Sample Stack' do
-  describe vpc('my-vpc') do
-    it { should exist }
-    it { should be_available }
-    its(:cidr_block) { should eq '10.0.0.0/16' }
-  end
-
-  describe subnet('my-subnet') do
-    it { should exist }
-    it { should be_available }
-    it { should belong_to_vpc('my-vpc') }
-  end
-
-  describe security_group('my-security-group') do
-    it { should exist }
-    it { should allow_in(port: 80, protocol: 'tcp') }
-    it { should allow_out(port: 443, protocol: 'tcp') }
-  end
-
-  describe ec2('my-ec2-instance') do
-    it { should exist }
-    it { should be_running }
-    it { should belong_to_subnet('my-subnet') }
-    it { should have_security_group('my-security-group') }
-    its(:instance_type) { should eq 't2.micro' }
+describe stack('sample-stack') do
+  it { should exist }
+  it { should be_running }
+  its('stack_status') { should eq 'CREATE_COMPLETE' }
+  its('template_description') { should include 'AWS CloudFormation Sample Template' }
+  its('parameters') do
+    should include(
+      {
+        'ParameterKey' => 'InstanceType',
+        'ParameterValue' => 't2.micro'
+      },
+      {
+        'ParameterKey' => 'KeyName',
+        'ParameterValue' => 'my-key-pair'
+      }
+    )
   end
 end
