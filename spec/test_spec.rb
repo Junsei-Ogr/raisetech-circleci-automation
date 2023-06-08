500042dcdbaa19d715d645fb2af10265a1843d37
EC2 Default User <ec2-user@ip-172-31-9-85.ap-northeast-1.compute.internal>
2023-06-08 14:46:57 +0000

    /spec/test_spec.rbの変更

--- spec/test_spec.rb
+++ spec/test_spec.rb
@@ -1,10 +1,29 @@
-require 'serverspec'
+require 'spec_helper'
 
-# テスト対象のサービスや設定などを記述します
-describe service('nginx') do
-  it { should be_running }
-end
+describe 'Sample Stack' do
+  describe vpc('my-vpc') do
+    it { should exist }
+    it { should be_available }
+    its(:cidr_block) { should eq '10.0.0.0/16' }
+  end
 
-describe port(80) do
-  it { should be_listening }
+  describe subnet('my-subnet') do
+    it { should exist }
+    it { should be_available }
+    it { should belong_to_vpc('my-vpc') }
+  end
+
+  describe security_group('my-security-group') do
+    it { should exist }
+    it { should allow_in(port: 80, protocol: 'tcp') }
+    it { should allow_out(port: 443, protocol: 'tcp') }
+  end
+
+  describe ec2('my-ec2-instance') do
+    it { should exist }
+    it { should be_running }
+    it { should belong_to_subnet('my-subnet') }
+    it { should have_security_group('my-security-group') }
+    its(:instance_type) { should eq 't2.micro' }
+  end
 end
